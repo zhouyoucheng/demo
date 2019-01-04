76905081170d0acc1bfd68d91b00839821ea0588
wb-zyc501131 <wb-zyc501131@alibaba-inc.com>
2019-01-04 14:10:55 +0800

    "test"

---
+++ Demo/src/main/java/test1/Format.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Format {
+	public static void main(String[] args) {
+		Object aObject = null;
+		Integer bInteger = null;
+		System.out.println(111);
+		System.out.println();
+		System.out.println(bInteger);
+		System.out.println(111);
+	}
+}
---
+++ Demo/src/main/java/test1/LocStockVO.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class LocStockVO {
+
+	public Object getAllocateStockQty() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getRealAvailableStock() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getRealStockQty() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
